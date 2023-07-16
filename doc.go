37dd67a3eec68c9f66d5e041c9cfdb24de02d6e0
srijanya yarram <100142338+Syar123@users.noreply.github.com>
2023-07-15 21:40:15 -0500

    adding more code description and doc file

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright © 2023 Srijanya Yarram. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Convert Apache Avro schema (this package supports schemas with array/record types) to BigQuery Table Schema.
+*/
+
+package main // import "golang.org/x/tools/cmd/godoc"
