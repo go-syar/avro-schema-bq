7c90aa0230a47ac8263333c7a0cb98bd62ab46bd
srijanya yarram <100142338+Syar123@users.noreply.github.com>
2023-07-16 20:43:43 -0500

    added a test case and a doc description

--- doc.go
+++ doc.go
@@ -3,7 +3,23 @@
 // license that can be found in the LICENSE file.
 
 /*
-Convert Apache Avro schema (this package supports schemas with array/record types) to BigQuery Table Schema.
+Convert Apache Avro schema to BigQuery Table Schema.
+This Package supports Avro schema with Record/Array data types as well.
+
+# Import Package
+
+-- go install github.com/go-syar/avro-schema-bq@latest
+
+# Create BQ Table with Avro Schema (avsc)
+
+Required arguments projectID, datasetID, tableID, serviceAccount, schemaFilePath
+
+-- table.CreateBQTableWithSA(projectID string, datasetID string, tableID string, serviceAccount string, schemaFilePath string) error
+// service account := "service-account.json"
+
+# Avro Schema (avsc) to BQ Schema (json)
+
+schema.ConvertAvroToBigQuery(avroSchema map[string]interface{}) ([]*bigquery.FieldSchema, error)
 */
 
 package main // import "golang.org/x/tools/cmd/godoc"
---
+++ schema_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/go-syar/avro-schema-bq/schema"
+)
+
+func TestConvertAvroToBigQuery(t *testing.T) {
+	// Test data: Avro schema in JSON format
+	avroSchemaJSON := `
+	{
+		"type": "record",
+		"name": "Person",
+		"fields": [
+			{"name": "Name", "type": "string"},
+			{"name": "Age", "type": "int"},
+			{"name": "Address", "type": "string"}
+		]
+	}`
+
+	// Convert the JSON Avro schema to a byte slice
+	avroSchemaContent := []byte(avroSchemaJSON)
+
+	// Unmarshal the Avro schema
+	var avroSchema map[string]interface{}
+	err := json.Unmarshal(avroSchemaContent, &avroSchema)
+	if err != nil {
+		t.Fatalf("Error parsing Avro schema: %v", err)
+	}
+
+	// Call the ConvertAvroToBigQuery function
+	bqFields, err := schema.ConvertAvroToBigQuery(avroSchema)
+	if err != nil {
+		t.Fatalf("Error converting Avro to BigQuery schema: %v", err)
+	}
+
+	// Test the result
+	expectedFields := bigquery.Schema{
+		{Name: "Name", Type: "STRING"},
+		{Name: "Age", Type: "INTEGER"},
+		{Name: "Address", Type: "STRING"},
+	}
+
+	if len(bqFields) != len(expectedFields) {
+		t.Fatalf("Expected %d fields, but got %d", len(expectedFields), len(bqFields))
+	}
+
+	for i, field := range bqFields {
+		if field.Name != expectedFields[i].Name || field.Type != expectedFields[i].Type {
+			t.Fatalf("Field %d: Expected (%s, %s), but got (%s, %s)", i, expectedFields[i].Name, expectedFields[i].Type, field.Name, field.Type)
+		}
+	}
+}
